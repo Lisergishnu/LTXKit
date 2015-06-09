66f359baf1372f80cffa2f3263ec7bc1115e9dce
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-06-08 22:50:34 -0300

    Changed headers

--- cylLTX.py
+++ cylLTX.py
@@ -3,24 +3,24 @@
 # @Author: Marco Benzi <marco.benzi@alumnos.usm.cl>
 # @Date:   2015-06-08 16:13:41
 # @Last Modified 2015-06-08
-# @Last Modified time: 2015-06-08 16:14:49
+# @Last Modified time: 2015-06-08 22:50:20
 
-""""
-==========================================================================
-This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+# ==========================================================================
+# This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-==========================================================================
-"""
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ==========================================================================
+
 
 import math
 
--- rectLTX.py
+++ rectLTX.py
@@ -3,24 +3,24 @@
 # @Author: Marco Benzi <marco.benzi@alumnos.usm.cl>
 # @Date:   2015-06-08 17:35:52
 # @Last Modified 2015-06-08
-# @Last Modified time: 2015-06-08 19:56:48
+# @Last Modified time: 2015-06-08 22:50:03
 
-""""
-==========================================================================
-This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+# ==========================================================================
+# This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-==========================================================================
-"""
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ==========================================================================
+
 
 import math
 
